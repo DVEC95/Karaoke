9eaad057088b05f00a731729263937a41295976f
David Eastcroft <davideastcroft95@gmail.com>
2020-03-20 16:42:30 +0000

    Adds tests for getting songs and guests.

--- specs/room_spec.rb
+++ specs/room_spec.rb
@@ -3,6 +3,8 @@ require('minitest/reporters')
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 require_relative('../room.rb')
+require_relative('../song.rb')
+require_relative('../guest.rb')
 
 class TestRoom < Minitest::Test
 
@@ -12,6 +14,14 @@ class TestRoom < Minitest::Test
 
   def test_get_room_number
     assert_equal("Bowie", @room.room_name)
+  end
+
+  def test_get_guests
+    assert_equal(0, @room.guests.size)
+  end
+
+  def test_get_songs
+    assert_equal(0, @room.songs.size)
   end
 
 end
