d6033bcfe875953d70808670031fe13b5d0c727f
David Eastcroft <davideastcroft95@gmail.com>
2020-03-20 17:20:14 +0000

    Adds max capacity variable.

--- room.rb
+++ room.rb
@@ -1,12 +1,14 @@
 class Room
 
-  attr_reader :room_name, :guests, :songs
+  attr_reader :room_name, :guests, :songs, :max_capacity
 
-  def initialize(room_name)
+  def initialize(room_name, max_capacity)
     @room_name = room_name
     @guests = []
     @songs = []
+    @max_capacity = max_capacity
   end
+
 
   def add_guests(guests)
     for guest in guests
