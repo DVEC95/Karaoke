7b77aa4ae00d733603f26796e472a5abea363f67
David Eastcroft <davideastcroft95@gmail.com>
2020-03-20 16:32:31 +0000

    Adds tests for getting song artist, title and release year.

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -10,4 +10,16 @@ class TestSong < Minitest::Test
     @song = Song.new("Purple Rain", "Prince", 1984)
   end
 
+  def test_get_song_title
+    assert_equal("Purple Rain", @song.title)
+  end
+
+  def test_get_song_artist
+    assert_equal("Prince", @song.artist)
+  end
+
+  def test_get_song_release
+    assert_equal(1984, @song.release)
+  end
+
 end
