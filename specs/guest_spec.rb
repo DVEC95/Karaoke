0ecf59589f3f86641dc7f279f2ad4c2c52b1a69b
David Eastcroft <davideastcroft95@gmail.com>
2020-03-22 01:48:03 +0000

    Adds tests for wallet and favourite song.

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -7,11 +7,19 @@ require_relative('../guest.rb')
 class TestGuest < Minitest::Test
 
   def setup
-    @guest = Guest.new("Bjork")
+    @guest = Guest.new("Ozzy", 15, "Iron Man")
   end
 
   def test_get_guest_name
-    assert_equal("Bjork", @guest.name)
+    assert_equal("Ozzy", @guest.name)
+  end
+
+  def test_get_wallet
+    assert_equal(15, @guest.wallet)
+  end
+
+  def test_get_fav_song
+    assert_equal("Iron Man", @guest.fav_song)
   end
 
 end
