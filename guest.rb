0d3915fce60713d87774a3464bcf81bf11a46797
David Eastcroft <davideastcroft95@gmail.com>
2020-03-22 01:36:46 +0000

    Adds wallet.

--- guest.rb
+++ guest.rb
@@ -1,9 +1,12 @@
 class Guest
 
-attr_reader :name
+attr_reader :name, :wallet
+attr_writer :wallet
 
-def initialize(name)
+def initialize(name, wallet)
   @name = name
+  @wallet = wallet
 end
+
 
 end
