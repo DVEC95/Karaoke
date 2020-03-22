0266915907eebe68731000eb6fd8eef8f6e50883
David Eastcroft <davideastcroft95@gmail.com>
2020-03-22 01:47:40 +0000

    Adds favourite song.

--- guest.rb
+++ guest.rb
@@ -1,12 +1,12 @@
 class Guest
 
-attr_reader :name, :wallet
+attr_reader :name, :wallet, :fav_song
 attr_writer :wallet
 
-def initialize(name, wallet)
+def initialize(name, wallet, fav_song)
   @name = name
   @wallet = wallet
+  @fav_song = fav_song
 end
-
 
 end
