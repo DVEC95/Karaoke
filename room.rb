e81ec26bd84f91ff5b9ef2c4c7f75ae46670398a
David Eastcroft <davideastcroft95@gmail.com>
2020-03-22 01:37:45 +0000

    Adds entry fee to room and updates add guest function.

--- room.rb
+++ room.rb
@@ -1,19 +1,29 @@
 class Room
 
-  attr_reader :room_name, :guests, :songs, :max_capacity
+  attr_reader :room_name, :guests, :songs, :max_capacity, :entry_fee
 
-  def initialize(room_name, max_capacity)
+  def initialize(room_name, max_capacity, entry_fee)
     @room_name = room_name
     @guests = []
     @songs = []
     @max_capacity = max_capacity
+    @entry_fee = entry_fee
   end
 
 
   def add_guests(guests)
-    for guest in guests
-      @guests.push(guests)
+    if guests.size <= @max_capacity
+      for guest in guests
+        if guest.wallet >= @entry_fee
+          charge_guest(guest)
+          @guests.push(guest)
+        end
+      end
     end
+  end
+
+  def charge_guest(guest)
+    guest.wallet -= @entry_fee
   end
 
   def remove_guests()
